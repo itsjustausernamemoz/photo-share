2dee1e489e40c62decb7c4066143566964046295
Wilbard Lazarus <113067290+itsjustausernamemoz@users.noreply.github.com>
2023-07-06 02:43:54 +0200

    Changes to be committed: 	modified:   routes/UsersRouter.js
     Changes not staged for commit:
    	modified:   models/UsersModel.js
    	modified:   node_modules/.package-lock.json
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   server.js
    
     Untracked files:
    	node_modules/bcryptjs/
    	node_modules/express-session/
    	node_modules/on-headers/
    	node_modules/random-bytes/
    	node_modules/uid-safe/
    	public/images/1688529696896--image.jpg
    	routes/PageRouter.js
    	views/login.ejs
    	views/signUp.ejs

--- routes/UsersRouter.js
+++ routes/UsersRouter.js
@@ -4,31 +4,60 @@ const db = require('../models');
 const bodyParser = require('body-parser');
 UsersRouter.use(bodyParser.urlencoded())
 UsersRouter.use(bodyParser.json())
+const bcrypt = require('bcryptjs');
+const saltRounds = 10;
 //login
 UsersRouter.route('/login')
 .post((request, response) => {
     const password = request.body.password
     const username = request.body.username
-    db.users.findOne({where: {username: username, password: password}}.then(user=>{
-            response.send(user)
+    db.user.findOne({where: {username: username, password: password}}
+        .then(async (user) =>{
+            if (user){
+                bcrypt.compare(password, user.password, (error, same) => {
+                    if(same){
+                        console.log("logged in, user ID =", user.id)
+                        request.session.userId = user.id
+                        response.redirect('/')
+                    }else{
+                        response.redirect('/login')
+                    }
+                })
+            }
         }).catch((error) => {
             response.send("You do not have an account. Try signing up!")
         }
     )
 )})//signup
-UsersRouter.route('/signup')
-.post((request, response) => {
+UsersRouter.route('/signUp')
+.post(async(request, response)=>{
+    // email, password, username
+    const email = request.body.email
+    const password = request.body.password
+    const encryptedPassword = await bcrypt.hash(password, saltRounds);
+    const username = request.body.username
+
+    db.user.create({email: email, password: encryptedPassword, username: username}).then(user=>{
+      //response.send(user) // changed in chapter 7.2
+      response.redirect('/login');
+    }).catch((error) => {
+        response.send("You do not have an account. Try signing up!")
+    })
+})
+/* UsersRouter.route('/signup')
+.post(async (request, response) => {
     const email = request.body.email
     const password = request.body.password
     const username = request.body.username
-    db.users.findOne({email: email, username: username, password: password}.then(user=>{
-            response.send(user)
+    const encryptedPassword = await bcrypt.hash(password, saltRounds);
+    db.user.create({email: email[0], username: username, password: password}.then(user=>{
+           // response.send(user)
+           response.redirect('/login');
         }).catch((error) => {
             response.send("You do not have an account. Try signing up!")
         }
     )
-)})
-
+)}) */
 
 
 
